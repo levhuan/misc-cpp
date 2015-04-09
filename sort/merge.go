dc7db2b3b2b1d8707fe08a4e671f4edda38f35bb
Huan Le <mr.huanle@gmail.com>
2015-04-09 10:54:23 -0700

    Separate Quick and Merge sort module
    Merge and Inversion count are in merge.go module
    Quick and RSelect count in the quick.go module

--- sort/merge.go
+++ sort/merge.go
@@ -120,99 +120,6 @@ func MergeCountInversion(input []int) ([]int, int) {
 	return output, my_inversion + left_inversion + right_inversion;
 }
 
-/*
- * Quick merge functions.
- * Quick merge includes:
- * - selectPivot
- * - partitions:
- * - recurse the left partition
- * - recurse the right partition
- */
-
-// moveToLeft moves "from" to the left of "pivot"
-func moveToLeft(input []int, pivot, from int) int {
-	temp := input[from]
-	input[from] = input[pivot + 1]
-	input[pivot + 1] = input[pivot]
-	input[pivot] = temp
-	return pivot + 1
-}
-
-// moveToRight moves "from" to the right of "pivot"
-func moveToRight(input []int, pivot, from int) int {
-	temp := input[from]
-	input[from] = input[pivot-1]
-	input[pivot-1] = input[pivot]
-	input[pivot] = temp
-	return pivot - 1
-}
-
-// selectPivot returns the index and value of the pivot
-//
-func selectRandomPivot(input []int) int {
-	return rand.Intn(len(input))
-}
-
-func selectPivot(input []int) int {
-	length := len(input)
-	first := input[0]
-	mid := input[length/2]
-	last := input[length - 1]
-
-	if first > last  &&  first < mid {
-		return 0
-	}
-	if last > first  &&  last < mid {
-		return length - 1
-	}
-	return length/2
-}
-
-func partition(input []int, pindex int) int {
-	pivot := input[pindex]
-	for i := 0; i < len(input); {
-		value := input[i]
-		if pivot < value  &&  pindex > i {
-			pindex = moveToRight(input, pindex, i)
-		} else if pivot > value  &&  pindex < i {
-			pindex = moveToLeft(input, pindex, i)
-		} else {
-			// advance to next element only when not swapping as
-			// swapping can move new value to the current index
-			//
-			// for duplicated value, don't need to swap as it will
-			// be moved to its rightful position in subsequent recursions
-			i++;
-		}
-	}
-
-	return pindex
-}
-
-// Quick sorts the input in-place by choosing a pivot and move the pivot to
-// it location/index in the slice. It recursively works on the left- and right-
-// sub-array of the pivot index until the length of the sub-arrays are zero.
-//
-// Average performance complexity: O(nlog(n))
-// Memory performance: O(1) - in-place
-//
-func Quick(input []int) {
-
-	num_qsort_recursion++
-
-	length := len(input)
-	if length <= 1 {
-		return 
-	}
-
-	pindex := selectRandomPivot(input)
-
-	pindex = partition(input, pindex)
-
-	Quick(input[0:pindex])
-	Quick(input[pindex + 1: length])
-}
-
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 	input := make([]int, rand.Intn(MAX_INPUT_LENGTH))
@@ -236,12 +143,5 @@ func main() {
 
 	fmt.Println("Count Inversions: ", output)
 	fmt.Println("Count recursion: ", num_count_recursion, " inversions: ", inversions,
-		"Perf: ", merge_perf)
-
-	stamp = time.Now().UTC().UnixNano()
-	Quick(input)
-	merge_perf = time.Now().UTC().UnixNano() - stamp
-	fmt.Println("Q-sorted:     ", input)
-	fmt.Println("Quick recursion: ", num_qsort_recursion,
 		"Perf: ", merge_perf)
 }
---
+++ sort/quick.go
@@ -0,0 +1,182 @@
+/*
+ * Block-style comment is for the package documentation.
+ * 
+ * Huan Le, mr.huanle@gmail.com 
+ */
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+const (
+	MAX_INPUT_LENGTH = 50
+	MAX_INPUT_VALUE  = 2000
+)
+
+var (
+	num_qsort_recursion int = 0
+	num_rselect_recursion int = 0
+)
+
+/*
+ * Quick merge functions.
+ * Quick merge includes:
+ * - selectPivot
+ * - partitions:
+ * - recurse the left partition
+ * - recurse the right partition
+ */
+
+// moveToLeft moves "from" to the left of "pivot"
+func moveToLeft(input []int, pivot, from int) int {
+	temp := input[from]
+	input[from] = input[pivot + 1]
+	input[pivot + 1] = input[pivot]
+	input[pivot] = temp
+	return pivot + 1
+}
+
+// moveToRight moves "from" to the right of "pivot"
+func moveToRight(input []int, pivot, from int) int {
+	temp := input[from]
+	input[from] = input[pivot-1]
+	input[pivot-1] = input[pivot]
+	input[pivot] = temp
+	return pivot - 1
+}
+
+// selectPivot returns the index and value of the pivot
+//
+func selectRandomPivot(input []int) int {
+	return rand.Intn(len(input))
+}
+
+func selectPivot(input []int) int {
+	length := len(input)
+	first := input[0]
+	mid := input[length/2]
+	last := input[length - 1]
+
+	if first > last  &&  first < mid {
+		return 0
+	}
+	if last > first  &&  last < mid {
+		return length - 1
+	}
+	return length/2
+}
+
+func partition(input []int, pindex int) int {
+	pivot := input[pindex]
+	for i := 0; i < len(input); {
+		value := input[i]
+		if pivot < value  &&  pindex > i {
+			pindex = moveToRight(input, pindex, i)
+		} else if pivot > value  &&  pindex < i {
+			pindex = moveToLeft(input, pindex, i)
+		} else {
+			// advance to next element only when not swapping as
+			// swapping can move new value to the current index
+			//
+			// for duplicated value, don't need to swap as it will
+			// be moved to its rightful position in subsequent recursions
+			i++;
+		}
+	}
+
+	return pindex
+}
+
+// Quick sorts the input in-place by choosing a pivot and move the pivot to
+// it location/index in the slice. It recursively works on the left- and right-
+// sub-array of the pivot index until the length of the sub-arrays are zero.
+//
+// Average performance complexity: O(nlog(n))
+// Memory performance: O(1) - in-place
+//
+func Quick(input []int) {
+
+	num_qsort_recursion++
+
+	length := len(input)
+	if length <= 1 {
+		return 
+	}
+
+	pindex := selectRandomPivot(input)
+
+	pindex = partition(input, pindex)
+
+	Quick(input[0:pindex])
+	Quick(input[pindex + 1: length])
+}
+
+// RSelect returns the i'th order value in the input array
+// (after sorted)
+//
+func RSelect(input []int, order int) int {
+
+	num_rselect_recursion++
+
+	length := len(input)
+	if length == 1 {
+		if order != 0 {
+			fmt.Println("Error: input ", input, " length ", length, " order ", order)
+		}
+		return input[0]
+	}
+
+	pindex := partition(input, selectRandomPivot(input));
+
+	switch {
+		case pindex > order:
+			return RSelect(input[0:pindex], order)
+		case pindex < order:
+			return RSelect(input[pindex+1:len(input)], order - pindex - 1)
+		default:
+			return input[pindex]
+	}
+
+}
+
+/*
+ * Main test Quick and RSelect functions 
+ */
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	input := make([]int, rand.Intn(MAX_INPUT_LENGTH))
+	input_clone := make([]int, len(input))
+	for i,_ := range input {
+		input[i] = rand.Intn(MAX_INPUT_VALUE)
+		input_clone[i] = input[i]
+	}
+
+	fmt.Println("Input:        ", input)
+
+	stamp := time.Now().UTC().UnixNano()
+	Quick(input)
+	merge_perf := time.Now().UTC().UnixNano() - stamp
+	fmt.Println("Q-sorted:     ", input)
+	fmt.Println("Quick recursion: ", num_qsort_recursion,
+		"Perf: ", merge_perf)
+	
+	stamp = time.Now().UTC().UnixNano()
+	data := RSelect(input_clone, 3)
+	merge_perf = time.Now().UTC().UnixNano() - stamp
+
+	fmt.Println("RSelect:     ", input_clone, " 3rd order: ", data)
+	fmt.Println("RSelect recursion: ", num_rselect_recursion,
+		"Perf: ", merge_perf)
+
+	num_rselect_recursion = 0
+	stamp = time.Now().UTC().UnixNano()
+	data = RSelect(input_clone, len(input_clone) - 1)
+	merge_perf = time.Now().UTC().UnixNano() - stamp
+
+	fmt.Println("RSelect:     ", input_clone, " Last order: ", data)
+	fmt.Println("RSelect recursion: ", num_rselect_recursion,
+		"Perf: ", merge_perf)
+}
